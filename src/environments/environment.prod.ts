58ca2fbfa4349df340414cbf31eaeddcb7fa75be
Ankit Gupta <ankit.gupta141189@gmail.com>
2019-08-14 18:29:02 -0700

    removing API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCLDLCxDwbnoxTotj1QXt6j4AvUKJuBJIg",
-    authDomain: "firestore-stackoverflow.firebaseapp.com",
-    databaseURL: "https://firestore-stackoverflow.firebaseio.com",
-    projectId: "firestore-stackoverflow",
-    storageBucket: "firestore-stackoverflow.appspot.com",
-    messagingSenderId: "427546524838",
-    appId: "1:427546524838:web:12759009528b0925"
+
   } 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,21 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCLDLCxDwbnoxTotj1QXt6j4AvUKJuBJIg",
-    authDomain: "firestore-stackoverflow.firebaseapp.com",
-    databaseURL: "https://firestore-stackoverflow.firebaseio.com",
-    projectId: "firestore-stackoverflow",
-    storageBucket: "firestore-stackoverflow.appspot.com",
-    messagingSenderId: "427546524838",
-    appId: "1:427546524838:web:12759009528b0925"
+
   } 
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
